19f1becfcbb9466c2e92f28a5f247591a689a366
md <karlistapc@gmail.com>
2020-10-23 11:22:06 +0200

    v2

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.calculadora
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun pulsarelboton1(view: View) {
+        EditText.
+    }
+    fun pulsarelboton2(view: View) {}
+    fun pulsarelboton3(view: View) {}
+    fun pulsarsuma(view: View) {}
+    fun pulsarboton7(view: View) {}
+    fun signo(view: View) {}
+    fun pulsarbotonocho(view: View) {}
+    fun pulsarboton9(view: View) {}
+    fun multiplicalo(view: View) {}
+    fun pulsarboton4(view: View) {}
+    fun pulsarboton5(view: View) {}
+    fun pulsarboton6(view: View) {}
+    fun pulsarelboton0(view: View) {}
+    fun dividelo(view: View) {}
+    fun ponenlacoma(view: View) {}
+    fun igualalo(view: View) {}
+    fun patras(view: View) {}
+
 }
+
+
